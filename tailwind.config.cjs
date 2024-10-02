52e3cc55d12917b7b332a3f40ad6f3e16b425f35
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2024-10-01 22:52:11 -0600

    Primer version de mi portafolio

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "rgba(0, 170, 219, 1)",
+          secondary: "rgba(219, 49, 0, 1)",
+          "base-100": "rgba(20, 20, 20, 1)",
+        },
+      },
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+};
